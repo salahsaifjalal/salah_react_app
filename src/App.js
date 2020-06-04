8065d4cfeb49587b5da8714f0a30bc9b3193d0ac
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-06-05 02:25:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React App from Salahuddin Muhammad. Congratulation!
+          Welcome to React App from Salahuddin Muhammad. Congratulation!.....
 		  
         </p>
         <a
